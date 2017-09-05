ad9f60d7039ab311e92f00b6151fb4040373ea4d
이용홍 <allbegray@gmail.com>
2017-09-05 18:36:24 +0900

    커맨드 인자 유틸 추가

--- src/main/java/secl/BridgeEngineUtils.java
+++ src/main/java/secl/BridgeEngineUtils.java
@@ -3,9 +3,13 @@ package secl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import secl.common.BridgeEngineParameter;
+import secl.common.Execution;
+import secl.common.Office;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public abstract class BridgeEngineUtils {
 
@@ -19,6 +23,25 @@ public abstract class BridgeEngineUtils {
         String json = new String(Base64.getDecoder().decode(encoded.getBytes()));
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(json, BridgeEngineParameter.class);
+    }
+
+    public static List<String> buildBridgeEngineExecuteArguments(BridgeEngineParameter parameter, Execution execution, Office office, String filePath) {
+        List<String> args = new ArrayList<>();
+        args.add("-execution");
+        args.add(execution.name());
+        args.add("-shareId");
+        args.add(parameter.getShareId());
+        args.add("-projectId");
+        args.add(parameter.getProjectId());
+        args.add("-version");
+        args.add(parameter.getVersion());
+        args.add("-office");
+        args.add(office.name());
+        args.add("-file");
+        args.add(filePath);
+        args.add("-schema");
+        args.add("schema_data_batch");
+        return args;
     }
 
 }
