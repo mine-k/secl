efa463019a47f338c69adda708168614d10a6c80
이용홍 <allbegray@gmail.com>
2017-09-25 13:56:18 +0900

    파일 패턴 추가

---
+++ src/main/java/secl/common/FilePattern.java
@@ -0,0 +1,23 @@
+package secl.common;
+
+public enum FilePattern {
+
+    HomeProject("_project_result.xlsx"),
+    HomeSchedule("_schedule_result.xlsx"),
+    HomeSync("_sync_s_to_h_result.xml"),
+    HomeSyncLog("_sync_result.log"),
+    SiteProject("_project.xlsx"),
+    SiteSchedule("_schedule.xlsx"),
+    SiteSyncHeadOfficeToSite("_sync_h_to_s.xml"),
+    SiteSyncSiteToHeadOffice("_sync_s_to_h.xml");
+
+    private String pattern;
+
+    FilePattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+}
